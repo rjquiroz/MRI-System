7163d10475e12d619366c51dcc62f837d1ea486d
Ronald Quiroz <ronald.quiroz89@gmail.com>
2022-03-15 23:35:00 -0400

    first iteration of the MRI program. Just FXML files.

---
+++ src/mriSystem/main.java
@@ -0,0 +1,23 @@
+package mriSystem;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("mrifx.fxml"));
+        primaryStage.setTitle("MRI System");
+        primaryStage.setScene(new Scene(root, 1400, 680));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
